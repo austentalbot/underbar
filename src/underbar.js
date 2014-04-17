8e477f520fec92a9e5122648f2f3b7ec70d7a0df
Austen Talbot <austentalbot@gmail.com>
2014-04-17 19:07:48 -0400

    First commit. Added identity and last.

--- src/underbar.js
+++ src/underbar.js
@@ -8,6 +8,7 @@ var _ = {};
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -38,6 +39,9 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : 
+        n>=array.length ? array :
+        array.slice(array.length-n, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
