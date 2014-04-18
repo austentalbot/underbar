a0e67d096794e0721e8278409c9dac7750386246
Austen Talbot <austentalbot@gmail.com>
2014-04-17 22:40:17 -0400

    Added and completed "filter" for Underbar.

--- src/underbar.js
+++ src/underbar.js
@@ -82,6 +82,15 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+
+    _.each(collection, function(item, index) {
+      if (test(item)) {
+        results.push(item);
+      }
+    });
+
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
