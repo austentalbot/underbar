ab5fb7bfb517044bda43e8e4059b8da6f9fbaf68
Austen Talbot <austentalbot@gmail.com>
2014-04-17 22:27:00 -0400

    Added and completed "each" in Underbar.

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,17 @@ var _ = {};
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    //test if collection is array
+    if (Array.isArray(collection)) {
+      for (var i=0; i<collection.length; i++) {
+        iterator(collection[i], i, collection);    
+      }      
+    //otherwise treat as object  
+    } else {
+      for (var j in collection) {
+        iterator(collection[j], j, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
