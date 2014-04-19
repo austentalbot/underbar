708914530d5fa6592113ad11de351cb2cfaa4bd9
Austen Talbot <austentalbot@gmail.com>
2014-04-19 12:07:34 -0400

    Completed the "map" function of Underbar.

--- src/underbar.js
+++ src/underbar.js
@@ -124,6 +124,13 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results=[];
+
+    _.each(collection, function(val, key) {
+      results.push(iterator(val, key));
+    });
+
+    return results;
   };
 
   /*
